ed394a5423ab53cd08ac70e5397102112da37a8b
Siraj Khan <sirajrkhan@live.in>
2020-02-28 11:34:22 +0530

    fetch the data completed with fix for infinite loop

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,48 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedID: null
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then( response => {
+            const posts = response.data.slice(0,6);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Siraj'
+                }
+            })
+            this.setState({posts: updatedPosts})
+        });
+    }
+
+    selectedIDHandler = (id) => {
+        this.setState({selectedID: id})
+    } 
+
     render () {
+        const posts = this.state.posts;
+        const post = posts.map( post => { 
+            return <Post 
+                        key={post.id} 
+                        title={post.title} 
+                        author={post.author}
+                        clicked={() => this.selectedIDHandler(post.id)} />
+        })
+       
+
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {post}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedID} />
                 </section>
                 <section>
                     <NewPost />
